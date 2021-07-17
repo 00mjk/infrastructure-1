98cd1861d2f735bd448fe7a48d4cc794e7f1c80a
Mauricio Alejandro Martínez Pacheco <n_othing@hotmail.com>
2021-07-16 19:33:43 -0500

    Update cluster.tf

--- cluster.tf
+++ cluster.tf
@@ -32,6 +32,6 @@ output "cluster_ca_certificate" {
   value = module.gke_cluster.cluster_ca_certificate
 }
 
-output "cluster_ca_certificate" {
+output "endpoint" {
   value = module.gke_cluster.endpoint
 }
